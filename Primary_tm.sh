9811e18b6b3c25f0d528f70412f80e90a9817d12
ABOL_HACKER <abolfazlm3419.2@gmail.com>
2024-07-08 20:35:02 +0330

    Add files via upload

---
+++ Primary_tm.sh
@@ -0,0 +1,19 @@
+folder_remove() {
+cd $HOME
+}
+install() {
+echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then>         command_not_found_handle() {
+/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+fi
+PS1='\$ '" > /data/data/com.termux/files/usr/etc/zshrc
+echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+command_not_found_handle() {
+/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+fi
+PS1='\033[1m\[\e[31m\]\033[1m┌─[\[\e[33m\]\T\[\e[31m\]\033[1m]─────\033[1m\e[1;98m\[[\033[1m\033[34;1mABOL HACKER\033[31m]\033[1m\e[31m\033[1m───[\033[32m\#\033[31m]\033[31m\n|\n\033[1m\e[31m\033[1m└─[\[\e[31m\]\e[1;32m\W\[\e[1m\033[31m]\033[1m────╼ \033[36m❯❯❯\e[1;32m\033[1m '" > /data/data/com.termux/files/usr/etc/bash.bashrc
+}
+folder_remove
+install
+folder_remove
